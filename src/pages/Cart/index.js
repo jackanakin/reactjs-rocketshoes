fc0862f884c9cd1ffdc5e1c5bb5a87581469bc3b
Jardel Kuhn <jardelkuhn@gmail.com>
2019-09-07 20:06:25 -0300

    feature: finished cart.js

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,73 @@
 import React from 'react';
-
-// import { Container } from './styles';
+import {
+    MdRemoveCircleOutline,
+    MdAddCircleOutline,
+    MdDelete,
+} from 'react-icons/md';
+import { Container, ProductTable, Total } from './styles';
 
 export default function Cart() {
-  return (
-    <h1>Carringo</h1>
-  );
+    return (
+        <Container>
+            <ProductTable>
+                <thead>
+                    <tr>
+                        <th />
+                        <th>PRODUTO</th>
+                        <th>QTD</th>
+                        <th>SUBTOTAL</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <img
+                                src="https://static.netshoes.com.br/produtos/tenis-adidas-duramo-lite-2-0-masculino/36/COL-3586-036/COL-3586-036_detalhe1.jpg?resize=280:280"
+                                alt="Tênis"
+                            />
+                        </td>
+                        <td>
+                            <strong>Tenis </strong>
+                            <span>R$ 1239,00</span>
+                        </td>
+                        <td>
+                            <div>
+                                <button type="button">
+                                    <MdRemoveCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                                <input type="number" readOnly value={1} />
+                                <button type="button">
+                                    <MdAddCircleOutline
+                                        size={20}
+                                        color="#7159c1"
+                                    />
+                                </button>
+                            </div>
+                        </td>
+                        <td>
+                            <strong>R$ 4123,12</strong>
+                        </td>
+                        <td>
+                            <button type="button">
+                                <MdDelete size={20} color="#7159c1" />
+                            </button>
+                        </td>
+                    </tr>
+                </tbody>
+            </ProductTable>
+
+            <footer>
+                <button type="button">Finalizar pedido</button>
+
+                <Total>
+                    <span>TOTAL</span>
+                    <strong>R$ 123,00</strong>
+                </Total>
+            </footer>
+        </Container>
+    );
 }
